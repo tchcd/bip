a15ebb1be96c0b51348dababe6b45c7df7bf636e
Petrov Nikita <n.petrov@lucky-team.pro>
2023-05-20 17:04:47 +0300

    linked list task main done

---
+++ 1_linked_list/linked_list_main.py
@@ -0,0 +1,110 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item: Node) -> None:
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self) -> None:
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val) -> list:
+        found_nodes = []
+        node = self.head
+
+        while node is not None:
+            if node.value == val:
+                found_nodes.append(node)
+            node = node.next
+
+        if not found_nodes:
+            return []
+        return found_nodes
+
+    def delete(self, val, all=False):
+        node = self.head
+        prev = None
+
+        if self.head is None:
+            return
+
+        while node and node.value == val:
+            self.head = node.next
+            node = node.next
+            if node is None:
+                self.tail = None
+            if not all:
+                return
+
+        while node is not None:
+            if node.value == val:
+                prev.next = node.next
+                if not all:
+                    return
+            else:
+                prev = node
+                self.tail = prev
+            node = node.next
+
+    def clean(self):
+        while self.head is not None:
+            prev = self.head
+            self.head = self.head.next
+            prev = None
+        self.tail = None
+        return
+
+    def len(self) -> int:
+        node = self.head
+        cnt = 0
+        while node is not None:
+            cnt += 1
+            node = node.next
+        return cnt
+
+    def insert(self, afterNode: Node, newNode: Node):
+        node = self.head
+
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+            return
+
+        if afterNode is None:
+            newNode.next = self.head
+            self.head = newNode
+            return
+
+        while node is not None:
+            if node == afterNode:
+                newNode.next = afterNode.next
+                afterNode.next = newNode
+                if newNode.next is None:
+                    self.tail = newNode
+            node = node.next
+        return
---
+++ 1_linked_list/linked_list_tests.py
@@ -0,0 +1,374 @@
+import unittest
+from linked_list_main import LinkedList, Node
+
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    @staticmethod
+    def _get_values(ll: LinkedList):
+        result = []
+        node = ll.head
+        while node is not None:
+            result.append(node.value)
+            node = node.next
+        return result
+
+    @staticmethod
+    def _create_empty_ll():
+        return LinkedList()
+
+    def test_find_all_first_middle_last(self):
+        ll = LinkedList()
+        n1 = Node(3)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(3)
+
+        ll.add_in_tail(n1)
+        ll.add_in_tail(n2)
+        ll.add_in_tail(n3)
+        ll.add_in_tail(n4)
+        ll.add_in_tail(n5)
+
+        self.assertEqual(ll.find_all(3), [n1, n3, n5])
+        self.assertEqual(ll.head, n1)
+        self.assertEqual(ll.tail, n5)
+
+    def test_find_all_empty_list(self):
+        empty_ll = LinkedList()
+        self.assertEqual(empty_ll.find_all(3), [])
+        self.assertEqual(empty_ll.head, None)
+        self.assertEqual(empty_ll.tail, None)
+
+    def test_find_all_one_el(self):
+        one_el_ll = LinkedList()
+        n0 = Node(0)
+        one_el_ll.add_in_tail(n0)
+        self.assertEqual(one_el_ll.find_all(0), [n0])
+        self.assertEqual(one_el_ll.head, n0)
+        self.assertEqual(one_el_ll.tail, n0)
+
+    ## Тесты для delete()
+    # + первая
+    # + последняя
+    # + пустой и один элемент
+    ## all = True.
+    # + две одинаковые подряд
+    # + две одинаковые через одну
+    # + одинаковые в начале и в конце
+    # + все одинаковые
+    # + не существующий
+
+    def test_delete_remove_first(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(1)
+        node3 = Node(2)
+        node4 = Node(3)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.add_in_tail(node4)
+
+        ll.delete(1)
+
+        self.assertEqual(self._get_values(ll), [1, 2, 3])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node2)
+        self.assertEqual(ll.tail, node4)
+
+    def test_delete_remove_last(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(1)
+        node3 = Node(2)
+        node4 = Node(3)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.add_in_tail(node4)
+
+        ll.delete(3)
+
+        self.assertEqual(self._get_values(ll), [1, 1, 2])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node3)
+
+    def test_delete_remove_from_empty_list(self):
+        ll = LinkedList()
+
+        ll.delete(3)
+
+        self.assertEqual(self._get_values(ll), [])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+
+    def test_delete_remove_from_one_el_list(self):
+        ll = LinkedList()
+        node1 = Node(0)
+        ll.add_in_tail(node1)
+
+        ll.delete(1)
+
+        self.assertEqual(self._get_values(ll), [0])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node1)
+
+    def test_delete_remove_being_from_one_el_list(self):
+        ll = LinkedList()
+        node1 = Node(0)
+        ll.add_in_tail(node1)
+
+        ll.delete(0)
+
+        self.assertEqual(self._get_values(ll), [])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+
+    def test_delete_remove_two_same_sequential(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(1)
+        node3 = Node(2)
+        node4 = Node(3)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.add_in_tail(node4)
+
+        ll.delete(1, all=True)
+
+        self.assertEqual(self._get_values(ll), [2, 3])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node3)
+        self.assertEqual(ll.tail, node4)
+
+    def test_delete_remove_same_through_one(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(1)
+        node4 = Node(3)
+        node5 = Node(1)
+        node6 = Node(4)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+        ll.add_in_tail(node4)
+        ll.add_in_tail(node5)
+        ll.add_in_tail(node6)
+
+        ll.delete(1, all=True)
+
+        self.assertEqual(self._get_values(ll), [2, 3, 4])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node2)
+        self.assertEqual(ll.tail, node6)
+
+    def test_delete_remove_same_seq_el(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(1)
+        node3 = Node(1)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+
+        ll.delete(1, all=True)
+
+        self.assertEqual(self._get_values(ll), [])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+
+    def test_delete_remove_not_being_el(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(1)
+        node3 = Node(1)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+
+        ll.delete(2, all=True)
+
+        self.assertEqual(self._get_values(ll), [1,1, 1])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node3)
+
+    def test_delete_remove_first_and_last(self):
+        ll = LinkedList()
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(1)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(node3)
+
+        ll.delete(1, all=True)
+
+        self.assertEqual(self._get_values(ll), [2])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.head, node2)
+        self.assertEqual(ll.tail, node2)
+
+    ## Тесты для clean()
+    # + пустой
+    # + один элемент
+    # + много одинаковых элементов
+
+    def test_clean_empty_list(self):
+        empty_ll = LinkedList()
+        empty_ll.clean()
+
+        self.assertEqual(self._get_values(empty_ll), [])
+        self.assertEqual(empty_ll, empty_ll)
+        self.assertEqual(empty_ll.tail, None)
+        self.assertEqual(empty_ll.head, None)
+
+    def test_clean_one_element_list(self):
+        one_el_ll = LinkedList()
+        n0 = Node(0)
+        one_el_ll.add_in_tail(n0)
+
+        one_el_ll.clean()
+
+        self.assertEqual(self._get_values(one_el_ll), [])
+        self.assertEqual(one_el_ll, one_el_ll)
+        self.assertEqual(one_el_ll.tail, None)
+        self.assertEqual(one_el_ll.head, None)
+
+    def test_clean_several_elements(self):
+        ll = LinkedList()
+        n1 = Node(3)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(3)
+
+        ll.add_in_tail(n1)
+        ll.add_in_tail(n2)
+        ll.add_in_tail(n3)
+        ll.add_in_tail(n4)
+        ll.add_in_tail(n5)
+
+        ll.clean()
+
+        self.assertEqual(self._get_values(ll), [])
+        self.assertEqual(ll, ll)
+        self.assertEqual(ll.tail, None)
+        self.assertEqual(ll.head, None)
+
+    ## Тесты для len()
+    # + пустой и один элемент
+    # + Передать много одинаковых элементов
+
+    def test_len_empty_list(self):
+        empty_ll = LinkedList()
+        self.assertEqual(empty_ll.len(), 0)
+        self.assertEqual(empty_ll.head, None)
+        self.assertEqual(empty_ll.tail, None)
+
+    def test_len_one_el_list(self):
+        n0 = Node(0)
+        one_el_ll = LinkedList()
+        one_el_ll.add_in_tail(n0)
+        self.assertEqual(one_el_ll.len(), 1)
+        self.assertEqual(one_el_ll.head, n0)
+        self.assertEqual(one_el_ll.tail, n0)
+
+    def test_len_several_el_list(self):
+        n0 = Node(0)
+        n1 = Node(2)
+        n2 = Node(4)
+        n3 = Node(4)
+        one_el_ll = LinkedList()
+        one_el_ll.add_in_tail(n0)
+        one_el_ll.add_in_tail(n1)
+        one_el_ll.add_in_tail(n2)
+        one_el_ll.add_in_tail(n3)
+        self.assertEqual(one_el_ll.len(), 4)
+        self.assertEqual(one_el_ll.head, n0)
+        self.assertEqual(one_el_ll.tail, n3)
+
+    ## Тесты для insert
+    # + пустой список (+afterNode = None)
+    # + один элемент в начало, в конец, (+afterNode = None)
+    # + перед последним
+    # + после последнего
+    # + в середину
+
+    def test_insert_empty_list_after_node_none(self):
+        empty_ll = LinkedList()
+        n1 = Node(1)
+
+        empty_ll.insert(afterNode=None, newNode=n1)
+        self.assertEqual(self._get_values(empty_ll), [1])
+        self.assertEqual(empty_ll.head, n1)
+        self.assertEqual(empty_ll.tail, n1)
+
+    def test_insert_to_start(self):
+        ll = LinkedList()
+        n1 = Node(1)
+        n0 = Node(0)
+        ll.add_in_tail(n1)
+
+        ll.insert(afterNode=n1, newNode=n0)
+        self.assertEqual(self._get_values(ll), [1, 0])
+        self.assertEqual(ll.head, n1)
+        self.assertEqual(ll.tail, n0)
+
+    def test_insert_to_start_after_none(self):
+        ll = LinkedList()
+        n1 = Node(1)
+        n0 = Node(0)
+        ll.add_in_tail(n1)
+
+        ll.insert(afterNode=None, newNode=n0)
+        self.assertEqual(self._get_values(ll), [0, 1])
+        self.assertEqual(ll.head, n0)
+        self.assertEqual(ll.tail, n1)
+
+    def test_insert_to_mid(self):
+        ll = LinkedList()
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n0 = Node(0)
+        ll.add_in_tail(n1)
+        ll.add_in_tail(n2)
+        ll.add_in_tail(n3)
+        ll.add_in_tail(n4)
+
+        ll.insert(afterNode=n3, newNode=n0)
+        self.assertEqual(self._get_values(ll), [1, 2, 3, 0, 4])
+        self.assertEqual(ll.head, n1)
+        self.assertEqual(ll.tail, n4)
+
+    def test_insert_to_end(self):
+        ll = LinkedList()
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n0 = Node(0)
+        ll.add_in_tail(n1)
+        ll.add_in_tail(n2)
+        ll.add_in_tail(n3)
+
+        ll.insert(afterNode=n3, newNode=n0)
+        self.assertEqual(self._get_values(ll), [1, 2, 3, 0])
+        self.assertEqual(ll.head, n1)
+        self.assertEqual(ll.tail, n0)
+
+
+if __name__ == "__main__":
+    unittest.main()
