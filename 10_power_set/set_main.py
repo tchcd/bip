0b3609d6467ed2b90f0acce00eaf80caed775814
Petrov Nikita <n.petrov@lucky-team.pro>
2023-07-16 13:26:05 +0300

    added hints to power_set

--- 10_power_set/set_main.py
+++ 10_power_set/set_main.py
@@ -2,32 +2,32 @@ class PowerSet:
     def __init__(self):
         self.slots = {}
 
-    def put(self, value):
+    def put(self, value) -> None:
         self.slots[value] = value
 
-    def size(self):
+    def size(self) -> int:
         return len(self.slots)
 
-    def get(self, value):
+    def get(self, value) -> bool:
         val = self.slots.get(value)
         if val is not None:
             return True
         return False
 
-    def remove(self, value):
+    def remove(self, value) -> bool:
         key = self.slots.pop(value, None)
         if key is not None:
             return True
         return False
 
-    def intersection(self, set2):
+    def intersection(self, set2: set) -> list:
         res = []
         for el in set2:
             if self.slots.get(el) is not None:
                 res.append(el)
         return res
 
-    def union(self, set2):
+    def union(self, set2: set) -> list:
         first_set = []
         second_set = []
 
@@ -42,14 +42,14 @@ class PowerSet:
 
         return union
 
-    def difference(self, set2):
+    def difference(self, set2: set) -> list:
         diff = []
         for el in self.slots.keys():
             if el not in set2:
                 diff.append(el)
         return diff
 
-    def issubset(self, set2):
+    def issubset(self, set2: set) -> bool:
         for el in set2:
             if self.slots.get(el) is None:
                 return False
