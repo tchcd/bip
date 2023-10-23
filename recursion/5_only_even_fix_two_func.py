042e2c9155ecc7f10f773d71e4522c3a9446ddb7
tchcd <techcode11@gmail.com>
2023-10-23 23:49:08 +0300

    fix 5, 6

--- recursion/5_only_even_fix_two_func.py
+++ recursion/5_only_even_fix_two_func.py
@@ -1,5 +1,5 @@
-def print_even_nums(array: list):
-	print_if_even(array, 0)
+def print_even_nums(array: list, start: int):
+	print_if_even(array, start)
 
 
 def print_if_even(array: list, idx: int):
@@ -13,6 +13,6 @@ def print_if_even(array: list, idx: int):
 
 
 if __name__ == '__main__':
-	print_even_nums([1, 2, 3, 4, 5, 6])
-	print_even_nums([-5, -4, -3, -2, -1])
-	print_even_nums([-3, -2, -1, 0, 1, 2, 3, 4, 5])
+	print_even_nums([1, 2, 3, 4, 5, 6], start=0)
+	print_even_nums([-5, -4, -3, -2, -1], start=0)
+	print_even_nums([-3, -2, -1, 0, 1, 2, 3, 4, 5], start=0)
