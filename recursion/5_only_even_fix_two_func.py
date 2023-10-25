b7b89246f28429e5635d06e7ef2e8b52f05946c0
tchcd <techcode11@gmail.com>
2023-10-25 09:05:45 +0300

    fix 5 6

--- recursion/5_only_even_fix_two_func.py
+++ recursion/5_only_even_fix_two_func.py
@@ -1,5 +1,6 @@
-def print_even_nums(array: list, start: int):
-	print_if_even(array, start)
+def print_even_nums(array: list):
+	start_idx = 0
+	print_if_even(array, start_idx)
 
 
 def print_if_even(array: list, idx: int):
@@ -13,6 +14,6 @@ def print_if_even(array: list, idx: int):
 
 
 if __name__ == '__main__':
-	print_even_nums([1, 2, 3, 4, 5, 6], start=0)
-	print_even_nums([-5, -4, -3, -2, -1], start=0)
-	print_even_nums([-3, -2, -1, 0, 1, 2, 3, 4, 5], start=0)
+	print_even_nums([1, 2, 3, 4, 5, 6])
+	print_even_nums([-5, -4, -3, -2, -1])
+	print_even_nums([-3, -2, -1, 0, 1, 2, 3, 4, 5])
--- recursion/6_only_even_idx_fix_two_func.py
+++ recursion/6_only_even_idx_fix_two_func.py
@@ -1,5 +1,6 @@
-def print_even_idx(array: list, start: int):
-	print_if_even(array, start)
+def print_even_idx(array: list):
+	start_idx = 0
+	print_if_even(array, start_idx)
 
 
 def print_if_even(array: list, idx: int):
@@ -12,9 +13,9 @@ def print_if_even(array: list, idx: int):
 
 
 if __name__ == '__main__':
-	print_even_idx([1, 2, 3, 4, 5, 6], start=0)					# 1, 3, 5
-	print_even_idx([-5, -4, -3, -2, -1], start=0)					# -5, -3, -1
-	print_even_idx([-3, -2, -1, 0, 1, 2, 3, 4, 5], start=0)		# -3, -1, 1, 3, 5
+	print_even_idx([1, 2, 3, 4, 5, 6])
+	print_even_idx([-5, -4, -3, -2, -1])
+	print_even_idx([-3, -2, -1, 0, 1, 2, 3, 4, 5])
 
 
 
